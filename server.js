9376f720ea2ce11f66b62d730b70c1e461374a72
unknown <annobilfrance@gmail.com>
2020-10-03 00:19:57 +0100

    adding backend with node

---
+++ server.js
@@ -0,0 +1,45 @@
+// we install express mongoose body-parser shortid
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const shortid = require("shortid");
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect(
+  "mongodb+srv://test:test@cluster0.d0ggr.mongodb.net/shoppingtest?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+const Product = mongoose.model(
+  '"products',
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+  })
+);
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
+app.delete("/api/product/:id", async (req, res) => {
+  const deleteProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server at port ${port}`));
